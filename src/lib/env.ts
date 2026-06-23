091e5246875f40b9e7b36fa18e67851576f046fa
erickabellaaluno <erickabella.aluno@unipampa.edu.br>
2026-06-23 18:08:10 -0300

    add api login route

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,12 +2,18 @@ export type EnvType = {
   db: {
     url: string
   }
+  jwt: {
+    secret: string
+  }
 }
 
 export default function getEnv(): EnvType {
   return {
     db: {
       url: process.env.DATABASE_URL!,
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET!,
     },
   }
 }
