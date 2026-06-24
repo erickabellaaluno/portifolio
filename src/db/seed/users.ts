d485075515f60e748f3384ddd8d5fd282745aa7f
erickabellaaluno <erickabella.aluno@unipampa.edu.br>
2026-06-24 00:20:28 -0300

    updates

--- src/db/seed/users.ts
+++ src/db/seed/users.ts
@@ -2,7 +2,7 @@ import { usersService } from '@/core/users.service'
 
 export async function seedUsers() {
   usersService.createUser({
-    email: 'admin@example.com',
-    plainPassword: 'password123',
+    email: 'admin@user.com',
+    plainPassword: '12345678',
   })
 }
