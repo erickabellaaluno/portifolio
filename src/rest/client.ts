34fecbac1c68c4b8398bafe3a77c9c7489b9fc80
erickabellaaluno <erickabella.aluno@unipampa.edu.br>
2026-06-26 00:08:45 -0300

    update api client

--- src/rest/client.ts
+++ src/rest/client.ts
@@ -1,10 +1,38 @@
 import getPublicEnv from '@/lib/env/public'
 import { contract } from '@/rest/contract'
-import { initClient } from '@ts-rest/core'
+import { ApiFetcherArgs, initClient, tsRestFetchApi } from '@ts-rest/core'
 
 const { baseUrl } = getPublicEnv()
 
 export const apiClient = initClient(contract, {
   baseUrl: baseUrl,
   baseHeaders: {},
+  api: async (args: ApiFetcherArgs) => {
+    try {
+      return await tsRestFetchApi({
+        ...args,
+        fetchOptions: {
+          ...args.fetchOptions,
+          next: {
+            revalidate: 60,
+          },
+        },
+      })
+    } catch {
+      return new Response(
+        JSON.stringify({
+          error: {
+            message: 'Internal Server Error',
+            code: -1,
+          },
+        }),
+        {
+          status: 500,
+          headers: {
+            'content-type': 'application/json',
+          },
+        },
+      )
+    }
+  },
 })
